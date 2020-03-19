9dd15d0043e010eb1098af57eae836438a4d6df2
a982338665 <982338665@qq.com>
2020-03-19 10:14:43 +0800

    支持表

--- routes/convertSql.js
+++ routes/convertSql.js
@@ -1,82 +1,114 @@
 const db = require('../util/mssql.js');
 const db2 = require('../util/mysql.js');
 
-function dealMsg(baseCreateSql, v, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, callback) {
-    let sql = baseCreateSql + v.name + fromCreateSql + v.name + ";";
-    let dropsql = basedropSql + v.name + suffixSql;
-    //1.视图转换为表
-    db.sql(sql, (err1, data1) => {
-        //2.获取表结构并转换
-        let execSql = exec + v.name + suffixSql;
-        db.sql(execSql, (err2, data2) => {
-            if (data2 && data2.recordset && data2.recordset.length > 0) {
-                //3.取出转换好的mysql建表语句
-                let mysqlSql = data2.recordset[0].sql;
-                if (mysqlSql != null) {
-                    //4.处理_0001
-                    mysqlSql = mysqlSql.replace(/_0001/g, '')
-                    //5.创建mysql数据表
-                    db2.query(mysqlSql, [], (err3, data3) => {
-                        if (!err3) {
-                            console.error("第" + i + "个：：：" + v.name + "  done！")
-                            callback(true, null)
-                            //6.查询视图数据 - 失败暂时不记录
-                            /*db.sql(selectSql + v.name + ";", (err4, data4) => {
-                                if (data4 && data4.recordset && data4.recordset.length > 0) {
-                                    //7.执行数据导入-单条导入，防止宕机
-                                    data4.recordset.forEach((v1,i1) => {
-                                        db2.query(insertSql+v.name+insertSqlSuffix,[v1],(err5,data5) => {
-                                            // console.error(err5)
-                                            // console.error(data5)
-                                        })
-                                    });
-                                }
-                            });*/
-                        } else {
-                            //已创建该表
-                            if ("ER_TABLE_EXISTS_ERROR" == err3.code) {
-                                console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  已创建")
-                                callback(true, '第 ' + i + '行id=' + v.id + '【' + v.name + "|已创建|" + '】' + err3.message);
-                            } else if ("ER_WRONG_AUTO_KEY" == err3.code) {
-                                console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  暂不支持！")
-                                callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|暂不支持|" + '】' + err3.message);
-                            } else {
-                                // console.error(err3.message+"|"+err3.code)
-                                console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  mysql表创建问题！" + err3.message)
-                                callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|mysql表创建问题|" + '】' + err3.message);
-                            }
-                        }
-                    });
-                } else {
-                    console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  建表语句转换问题！")
-                    callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|建表语句转换问题|" + '】' + mysqlSql);
-                }
+function dropTableMssql(baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, callback) {
+    //是视图的时候删除
+    if (type == 'V' || type == 'v') {
+        let dropsql = basedropSql + v.name + suffixSql;
+        db.sql(dropsql, (err, data) => {
+            if (err) {
+                callback(false, v.name, '第 ' + i + '行id=' + v.id + ' | ' + v.name + "删除失败：【" + err.code + "|" + err.message + "】")
             } else {
-                console.error('第 ' + i + '行id=' + v.id + '【' + v.name + " " + err2 == null ? 'NULL' : err2.message)
-                callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "||" + err2 == null ? 'NULL' : err2.message + '】');
+                callback(true, null, null)
             }
         })
+    } else {
+        callback(true, null, null)
+    }
+}
+
+function convertMysql(execSql, i, v, callback) {
+    db.sql(execSql, (err2, data2) => {
+        if (data2 && data2.recordset && data2.recordset.length > 0) {
+            //3.取出转换好的mysql建表语句
+            let mysqlSql = data2.recordset[0].sql;
+            if (mysqlSql != null) {
+                //4.处理_0001
+                mysqlSql = mysqlSql.replace(/_0001/g, '')
+                //5.创建mysql数据表
+                db2.query(mysqlSql, [], (err3, data3) => {
+                    if (!err3) {
+                        console.error("第" + i + "个：：：" + v.name + "  done！")
+                        callback(true, null)
+                        //6.查询视图数据 - 失败暂时不记录
+                        /*db.sql(selectSql + v.name + ";", (err4, data4) => {
+                            if (data4 && data4.recordset && data4.recordset.length > 0) {
+                                //7.执行数据导入-单条导入，防止宕机
+                                data4.recordset.forEach((v1,i1) => {
+                                    db2.query(insertSql+v.name+insertSqlSuffix,[v1],(err5,data5) => {
+                                        // console.error(err5)
+                                        // console.error(data5)
+                                    })
+                                });
+                            }
+                        });*/
+                    } else {
+                        //已创建该表
+                        if ("ER_TABLE_EXISTS_ERROR" == err3.code) {
+                            console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  已创建")
+                            callback(true, '第 ' + i + '行id=' + v.id + '【' + v.name + "|已创建|" + '】' + err3.message);
+                        } else if ("ER_WRONG_AUTO_KEY" == err3.code) {
+                            console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  暂不支持！")
+                            callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|暂不支持|" + '】' + err3.message);
+                        } else {
+                            // console.error(err3.message+"|"+err3.code)
+                            console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  mysql表创建问题！" + err3.message)
+                            callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|mysql表创建问题|" + '】' + err3.message);
+                        }
+                    }
+                });
+            } else {
+                console.error('第 ' + i + '行id=' + v.id + '【' + v.name + "  建表语句转换问题！")
+                callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "|建表语句转换问题|" + '】' + mysqlSql);
+            }
+        } else {
+            console.error('第 ' + i + '行id=' + v.id + '【' + v.name + " " + err2 == null ? 'NULL' : err2.message)
+            callback(false, '第 ' + i + '行id=' + v.id + '【' + v.name + "||" + err2 == null ? 'NULL' : err2.message + '】');
+        }
     })
 }
 
+function dealMsg(baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, callback) {
+    let sql = baseCreateSql + v.name + fromCreateSql + v.name + ";";
+    if (type == "U" || type == "u") {
+        //数据表直接转换
+        let execSql = exec + v.name + ";";
+        convertMysql(execSql, i, v, callback);
+    } else if (type == "V" || type == "v") {
+        //1.视图转换为表
+        db.sql(sql, (err1, data1) => {
+            //2.获取表结构并转换
+            let execSql = exec + v.name + suffixSql;
+            convertMysql(execSql, i, v, callback);
+        })
+    } else {
+        callback(false,"type类型错误")
+    }
+}
+
 //获取所有视图的视图名称
-const asyncAjax = function (baseCreateSql, v, fromCreateSql, basedropSql, suffixSql, exec, selectSql, msg, i, recordset) {
+const asyncAjax = function (baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, msg, dropmsg, i, recordset) {
     return new Promise(function (resolve, reject) {
-        dealMsg(baseCreateSql, v, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, (err, data) => {
-            if (!err) {
-                msg.push(data + ' <br/>');
-            }
-            resolve(1);
-            if (i === recordset.length - 1) {
-                console.log(msg);
-            }
+        dealMsg(baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, (err, data) => {
+            dropTableMssql(baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, i, (isnot, names, msgs) => {
+                if (!err) {
+                    msg.push(data + ' \n<br/>');
+                }
+                if (!isnot) {
+                    dropmsg.push(msgs + ' \n<br/>')
+                }
+                resolve(1);
+                if (i === recordset.length - 1) {
+                    console.log(msg);
+                }
+            });
         });
     });
 
 }
 
 //将视图转为表结构创建出来，不添加数据
-const dealSync = async function (data,callback) {
+const dealSync = async function (data, type, callback) {
     let basedropSql = `drop table `
     let suffixSql = `_0001; `
     let baseCreateSql = `select top 500 * into `
@@ -86,11 +118,12 @@ const dealSync = async function (data,callback) {
     let insertSql = "insert into ";
     let insertSqlSuffix = " set ? ";
     let msg = [];
+    let dropmsg = [];
     for (let i = 0; i < data.recordset.length; i++) {
         let v = data.recordset[i];
-        await asyncAjax(baseCreateSql, v, fromCreateSql, basedropSql, suffixSql, exec, selectSql, msg, i, data.recordset);
+        await asyncAjax(baseCreateSql, v, type, fromCreateSql, basedropSql, suffixSql, exec, selectSql, msg, dropmsg, i, data.recordset);
     }
-    callback(true,data.recordset.length,msg)
+    callback(true, data.recordset.length, msg, dropmsg)
 }
 
 // let sql = 'select TOP 0 * into v_BOM_01 from v_BOM;\n';
@@ -99,39 +132,44 @@ const dealSync = async function (data,callback) {
  * @param bool
  * @param viewName
  * @param callback
+ * type V-视图 U-表 P-存储过程 TR触发器
+ * 仅支持视图和表同步
  */
-function convertSql(bool, viewName, callback) {
+function convertSql(bool, viewName, type, callback) {
     //如果是true，获取所有视图转换
-    let getView = "select id,name from sysobjects where xtype='V' ";
+    let getView = "select id,name from sysobjects where xtype= " + "'" + type +
+        "'";
     if (!bool) {
         if (viewName) {
             getView = getView + " and name = '" + viewName + "'";
         } else {
-            callback(false, '缺少视图名称！',null)
+            callback(false, '缺少视图名称！', null, null)
         }
     }
     // console.error(getView)
     //查询视图名称
     db.sql(getView, (err, data) => {
         if (data && data.recordset && data.recordset.length > 0) {
-            dealSync(data,(err,data,msg) => {
-                callback(err,data,msg)
+            dealSync(data, type, (err, data, msg, dropmsg) => {
+                callback(err, data, msg, dropmsg)
             });
         } else {
-            callback(false, "暂无视图",null)
+            callback(false, "暂无视图", null, null)
         }
     })
 
 
 }
 
-convertSql(true, null,(isnot,data,msg) => {
-    if (isnot){
-        console.error("总共数据："+data)
-        console.error("创建成功："+(data-msg.length))
-        console.error("创建失败："+msg.length)
-        console.error("失败详情："+msg)
-    }else{
+convertSql(true, null, "V", (isnot, data, msg, dropmsg) => {
+    if (isnot) {
+        console.error("总共数据：" + data)
+        console.error("创建成功：" + (data - msg.length))
+        console.error("创建失败：" + msg.length)
+        console.error("失败详情：" + msg)
+        console.error("删除失败：" + dropmsg.length)
+        console.error("删除失败详情：" + dropmsg)
+    } else {
         console.error(data)
     }
 });
